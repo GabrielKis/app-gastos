fc6e753e9f42e490ceea9aff9430b195b5eca831
GabrielKis <gabrielkis65@gmail.com>
2024-06-17 16:46:51 -0300

    working

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 mod expense;
-//fn read_pdf_extract(filename: &str) {
-//    let bytes = std::fs::read(filename).unwrap();
-//    let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
-//    println!("{}", out);
-//}
-
-// regular expression: ^\d{2}\s[A-Z]{3}\s[a-zA-z\s]*\d{1,3},\d{2}$
-// match this thing: 
+use std::path::Path;
 
 fn main() {
-    let pdf_file = "tests/test_pdfs/Nubank_2024-04-20.pdf";
+    let project_path = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let tests_pdf_path = project_path.join("tests").join("test_pdfs");
+    let tests_csv_path = project_path.join("tests").join("test_csv");
+    let pdf_expense_filename = "Nubank_2024-06-22.pdf";
+    let csv_expense_filename = pdf_expense_filename.replace(".pdf", ".csv");
+
+    let mut pdf_expense_file_path = String::new();
+    let mut csv_expense_file_path = String::new();
+
+    match tests_pdf_path.join(pdf_expense_filename).to_str() {
+        Some(pdf_file) => pdf_expense_file_path = pdf_file.to_string(),
+        None => println!("Error"),
+    }
+
+    match tests_csv_path.join(csv_expense_filename).to_str() {
+        Some(csv_file) => csv_expense_file_path = csv_file.to_string(),
+        None => println!("Error"),
+    }
+
     let mut expenses_on_pdf: Vec<expense::ExpenseData> = Vec::new();
-    expense::read_income_pdf(&pdf_file, &mut expenses_on_pdf);
-    let _ = expense::convert_incomes_to_csv("tests/test_csv/Nubank_2024-04-20.csv", &mut expenses_on_pdf);
+    expense::read_income_pdf(&pdf_expense_file_path, &mut expenses_on_pdf);
+    let _ = expense::convert_incomes_to_csv(&csv_expense_file_path, &mut expenses_on_pdf);
+    println!("Arquivo CSV gerado em: {}", csv_expense_file_path);
 }
