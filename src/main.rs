11a313deb6c935810287ed317534f1eb21d47e3d
GabrielKis <gabrielkis65@gmail.com>
2024-06-19 12:17:45 -0300

    error handling on files

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod expense;
 use std::path::Path;
 use std::fs;
 
-use regex::Error;
 use slint::{format, SharedString};
 slint::include_modules!();
 
@@ -20,21 +19,19 @@ fn execute_conversion(pdf_filename: SharedString, csv_filename: SharedString) ->
     // check if csv path is valid (if not, create it)
     let csv_output_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("output").join("csv");
     match fs::create_dir_all(csv_output_path.clone()) {
-        Ok(_) => {} // Do nothing,
+        Ok(a) => println!("{:?}", a), // Do nothing,
         Err(e) => return Err(format!("Não foi possível criar o diretório de saida do CSV: {}", e))
     }
 
-    let mut csv_expense_file_path = String::new();
-    match csv_output_path.join(csv_filename.as_str()).to_str() {
-        Some(csv_file) => csv_expense_file_path = csv_file.to_string(),
-        None => return Err(format!("Não foi possível criar o arquivo CSV")),
-    }
+    // join CSV file name
+    let csv_expense_file_path_bind = csv_output_path.join(csv_filename.as_str());
+    let csv_expense_file_path = csv_expense_file_path_bind.to_str();
 
     let mut expenses_on_pdf: Vec<expense::ExpenseData> = Vec::new();
     expense::read_income_pdf(&pdf_filename.as_str(), &mut expenses_on_pdf);
-    let _ = expense::convert_incomes_to_csv(&csv_expense_file_path, &mut expenses_on_pdf);
+    let _ = expense::convert_incomes_to_csv(&csv_expense_file_path.unwrap(), &mut expenses_on_pdf);
 
-    Ok(format!("Arquivo CSV gerado em: {}", csv_expense_file_path))
+    Ok(format!("Arquivo CSV gerado em: {}", csv_expense_file_path.unwrap()))
 }
 
 fn main() -> Result<(), slint::PlatformError> {
